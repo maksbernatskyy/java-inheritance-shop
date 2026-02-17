12280899960dff99f85d91075a4fac163d5e0240
maksbernatskyy <maksbernatskyy@gmail.com>
2026-02-17 14:09:35 +0100

    add class Cuffie

--- src/org/lessons/java/shop/Cuffie.java
+++ src/org/lessons/java/shop/Cuffie.java
@@ -1,5 +1,43 @@
 package org.lessons.java.shop;
 
-public class Cuffie {
+import java.math.BigDecimal;
+
+class Cuffie extends Prodotto {
+    // Attributi
+    private String colore;
+
+    enum Tipologia {
+        WIRELESS,
+        CABLATE
+    }
+
+    private Tipologia tipologia;
+
+    // Metodi
+    public Cuffie(String nome, String marca, BigDecimal prezzo, BigDecimal iva, String colore, Tipologia tipologia) {
+        super(nome, marca, prezzo, iva);
+        this.colore = colore;
+        this.tipologia = tipologia;
+    }
+
+    // Colore
+    public String getColore() {
+        return this.colore;
+    }
+
+    public void setColore(String colore) {
+        if (colore != null && !colore.equals("")) {
+            this.colore = colore;
+        }
+    }
+
+    // Tipologia
+    public Tipologia getTipologia() {
+        return tipologia;
+    }
+
+    public void setTipologia(Tipologia tipologia) {
+        this.tipologia = tipologia;
+    }
 
 }
--- src/org/lessons/java/shop/Main.java
+++ src/org/lessons/java/shop/Main.java
@@ -2,11 +2,16 @@ package org.lessons.java.shop;
 
 import java.math.BigDecimal;
 
+import org.lessons.java.shop.Cuffie.Tipologia;
+
 public class Main {
     public static void main(String[] args) {
         Smarphone Iphone16Pro = new Smarphone("Iphone 16 Pro", "Apple", new BigDecimal("800"), new BigDecimal("22"), 22234567, 564);
 
         Televisori Samsung = new Televisori("Samsung", "Samsung", new BigDecimal("2000.00"), new BigDecimal("22"), 75, true);
+
+        Cuffie Beats = new Cuffie("Beats", "Beats", new BigDecimal("120"), new BigDecimal("22"), "Black", Tipologia.WIRELESS);
         
+
     }
 }
