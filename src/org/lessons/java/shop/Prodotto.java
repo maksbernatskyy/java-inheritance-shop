cd1f0d77521e50eb30ac01b5cf9b8f131f6641e2
maksbernatskyy <maksbernatskyy@gmail.com>
2026-02-17 12:32:24 +0100

    first commit

---
+++ src/org/lessons/java/shop/Prodotto.java
@@ -0,0 +1,12 @@
+package org.lessons.java.shop;
+
+import java.math.BigDecimal;
+
+public class Prodotto {
+    // Attributi
+    private int codice;
+    private String nome;
+    private String marca;
+    private BigDecimal prezzo;
+    private BigDecimal iva;
+}
